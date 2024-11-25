c1ec71502951692bc6983b8bf0ec33c299f76f95
SHAWN JONATHAN CHANDRA <i22021@ftis>
2024-11-25 10:14:16 +0700

    initial commit

---
+++ application/application/src/test/java/com/pbw/application/StravaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbw.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
