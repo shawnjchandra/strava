9202a4059946b2f433b56689a23cd80c64332536
Shawn <myking123321@gmail.com>
2024-12-10 23:14:24 +0700

    feat(login) : add: post mapping for login

--- application/application/src/main/java/com/pbw/application/controller/MainController.java
+++ application/application/src/main/java/com/pbw/application/controller/MainController.java
@@ -1,10 +1,23 @@
 package com.pbw.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.pbw.application.RequiredRole;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
     @GetMapping("/")
     public String homepageView(){
@@ -14,5 +27,25 @@ public class MainController {
     @GetMapping("/login")
     public String getLoginView(){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginForm(
+        @RequestParam(name = "email")String email,
+        @RequestParam(name = "password")String password,
+        Model Model, 
+        HttpSession httpSession){
+            
+            
+
+
+        return "dashboard";
+    }
+    
+
+    @GetMapping("/dashboard")
+    @RequiredRole({"*"})
+    public String getDashboardView(){
+        return "/dashboard";
     }
 }
