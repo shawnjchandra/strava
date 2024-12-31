8f2205ad43fbee8dd7ab98d6d5db7bb1a915b427
Shawn <myking123321@gmail.com>
2024-12-31 12:03:12 +0700

    mod: added storage service

--- application/application/src/main/java/com/pbw/application/StravaApplication.java
+++ application/application/src/main/java/com/pbw/application/StravaApplication.java
@@ -1,14 +1,28 @@
 package com.pbw.application;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.pbw.application.storage.StorageProperties;
+import com.pbw.application.storage.StorageService;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableConfigurationProperties(StorageProperties.class)
 public class StravaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StravaApplication.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService){
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();	
+		};
+	}
 }
