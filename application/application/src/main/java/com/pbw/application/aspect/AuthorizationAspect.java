15199872349aa5aa36797d82eb9ef3a8471c8943
Shawn <myking123321@gmail.com>
2025-01-04 22:03:57 +0700

    fix: authorization aspect update and redirect for unauthorized

--- application/application/src/main/java/com/pbw/application/aspect/AuthorizationAspect.java
+++ application/application/src/main/java/com/pbw/application/aspect/AuthorizationAspect.java
@@ -1,8 +1,10 @@
 package com.pbw.application.aspect;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import com.pbw.application.RequiredRole;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Aspect
@@ -17,26 +20,28 @@ import jakarta.servlet.http.HttpSession;
 public class AuthorizationAspect {
 
     private final HttpSession httpSession;
+    private final HttpServletResponse httpServletResponse;
 
-    
-    public AuthorizationAspect(HttpSession httpSession) {
+    public AuthorizationAspect(HttpSession httpSession, HttpServletResponse httpServletResponse) {
         this.httpSession = httpSession;
+        this.httpServletResponse = httpServletResponse;
     }
 
-    @Around("@annotation(requiredRole)")
+    @After("@annotation(requiredRole)")
     public void checkAuthorization(JoinPoint joinPoint
     , RequiredRole requiredRole
     ) throws Throwable{
 
 
-        String currentUsername = (String)httpSession.getAttribute("username");
+        // String currentUsername = (String)httpSession.getAttribute("username");
         
-        if(currentUsername == null){
-            throw new Throwable("User is not logged in");
-        }
+        // if(currentUsername == null){
+        //     throw new Throwable("User is not logged in");
+        // }
 
         String currentRole = (String)httpSession.getAttribute("role");
 
+        System.out.println("current role: "+currentRole);
 
         boolean isAllowed = false;
         for(String role : requiredRole.value()){
@@ -44,15 +49,22 @@ public class AuthorizationAspect {
                 isAllowed = true;
             }
         }
-
-        if(isAllowed == false){
-            throw new Throwable("Insufficient Permission");
+        
+        System.out.println("check: "+ isAllowed);
+        if(!isAllowed){
+            redirectToUnauthorized();
+            // throw new Throwable("Insufficient Permission");
         }
     }
 
     @Before("execution(* com.example.m08.LoginController.*(..))")
     public void log(){
         // System.out.println("EXECUTING METHOD IN LOGIN CONTROLLER ");
+    }
+
+    private void redirectToUnauthorized() throws IOException {
+        System.out.println("Redirecting to unauthorized");
+        httpServletResponse.sendRedirect("/unauthorized");
     }
 
 }
